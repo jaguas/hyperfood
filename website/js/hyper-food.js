d60e2cf06be3fae04aaff2701915d08cd1df5d80
Camila Vilarinho <camilaavilarinho@gmail.com>
2017-10-01 13:01:10 +0100

    calling the api

--- website/js/hyper-food.js
+++ website/js/hyper-food.js
@@ -1,18 +1,22 @@
-var product = {
-
- productId: "1234"
- waterPh: 7.5
- seeds: "Orange Type A"
- pesticides: "Organit Pest Control"
- soil: "clay soil type"
- fertilizer: "Grow Fast"
- origin: "Belgique"
- movementStatus: "ReadyForAudit"
-  // Producer producer
-  // Audit[] audits
-}
+var api = "http://13.85.83.174/explorer/";
 
 function search(productId)
 {
-  $("#fertilizers").val(product.fertilizer);
+  //$("#fertilizers").val(product.fertilizer);
+  $.get( "http://13.85.83.174/api/Product/" + productId, function( data ) {
+    console.log(data);
+    $("#seeds").html(data[0].seeds);
+    $("#pesticides").html(data[0].pesticides[0]);
+    $("#water").html(data[0].waterPh);
+    $("#soil").html(data[0].soil);
+    $("#fertilizers").html(data[0].fertilizer[0]);
+    $("#origin").html(data[0].origin);
+  });
 }
+
+$(function(){
+  $("#search-button").click(function() {
+    var productId = $("#search").val();
+    search(productId);
+  });
+});
