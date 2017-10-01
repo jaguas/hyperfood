9a8cbd5ab08fcbadc9d01017fc2bcba7acf34951
Camila Vilarinho <camilaavilarinho@gmail.com>
2017-10-01 10:45:34 +0100

    adding the website folder

---
+++ website/README.md
@@ -0,0 +1,3 @@
+# Hyper food
+
+Project page
---
+++ website/js/hyper-food.js
@@ -0,0 +1,18 @@
+var product = {
+
+ productId: "1234"
+ waterPh: 7.5
+ seeds: "Orange Type A"
+ pesticides: "Organit Pest Control"
+ soil: "clay soil type"
+ fertilizer: "Grow Fast"
+ origin: "Belgique"
+ movementStatus: "ReadyForAudit"
+  // Producer producer
+  // Audit[] audits
+}
+
+function search(productId)
+{
+  $("#fertilizers").val(product.fertilizer);
+}
